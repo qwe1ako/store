0b989aab365186fe79a817126bd0a444f4a64d84
qwe1ako <1097326802@qq.com>
2021-05-27 20:30:45 +0800

    1

---
+++ daima/the4day/paixu.py
@@ -0,0 +1,14 @@
+i=0
+while i<=7:
+    j=1
+    a=1
+    while a <= (7-i) :
+        print(" ",end="")
+        a = a + 1
+    while j<=i:
+        print("* ",end="")
+        j=j+1
+    print("*")
+    i=i+1
+
+

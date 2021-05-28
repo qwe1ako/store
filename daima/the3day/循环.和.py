721fc178e28427efb6c653766343eef3cc388721
qwe1ako <1097326802@qq.com>
2021-05-28 10:06:22 +0800

    2

---
+++ daima/the3day/循环.和.py
@@ -0,0 +1,16 @@
+'''
+number= 1
+while number <= 10 :
+    print(number)
+    number=number+1
+'''
+#while True:
+#    print(2)
+#和
+list=[1,2,3,4,5,6,7]
+count=0
+a=0
+while count<7:
+    a=a+list[count]
+    count=count+1
+print(a)
